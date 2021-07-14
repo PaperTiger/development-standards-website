c1b5201ed128474026bbc23267eb0ff3380c08fb
Kenyon Kowal <hello@kenyonkowal.com>
2021-07-14 11:58:20 -0400

    update

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,15 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  
   eleventyConfig.setTemplateFormats([
     "md",
     "css" // css is not yet a recognized template extension in Eleventy
   ]);
+
+  return {
+    dir: {
+      input: "src",
+      output: "_pages"
+    }
+  };
 };
